48cddcd508b515081413d95ba123e0028a405a4a
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2022-10-21 17:42:27 -0600

    Ejercicio2
    Realice ejercicio 2

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ verifique la lista de manera que retorne el número mayor. */
 
 const maxNumber = [1, 3, 80, 20, 5]
 console.log(Math.max(...maxNumber));
+
+/* 2. Cree un programa que reciba como parámetro una palabra y verifique si
+dicha palabra es palíndromo, es decir, la palabra se lee igual de izquierda a
+derecha o de derecha a izquierda, o no. Para la resolución de dicho problema
+debe utilizar ciclo for */
+
+function invertirCadena(cad) {
+    let nuevaCadena = "";
+    for (let i = cad.length - 1; i >= 0; i--) {
+        nuevaCadena += cad[i];
+    }
+    return nuevaCadena;
+}
+console.log(invertirCadena('Anna'));
