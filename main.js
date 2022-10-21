ac229e02a14bc5cfa170b242fc89ef7ceb9357bd
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2022-10-21 16:43:39 -0600

    Ejercicio1
    Realice ejercicio 1

---
+++ main.js
@@ -0,0 +1,5 @@
+/* 1. Cree un programa que reciba como parámetro un array de números, y
+verifique la lista de manera que retorne el número mayor. */
+
+const maxNumber = [1, 3, 80, 20, 5]
+console.log(Math.max(...maxNumber));
